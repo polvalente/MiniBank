a1a357cf909ed668e65046d179a9313c2e4aebb5
Paulo Valente <polvalente@gmail.com>
2017-11-01 17:43:11 -0200

    Added account history feature

--- Domains/Entities/Account.py
+++ Domains/Entities/Account.py
@@ -1,21 +1,37 @@
+from MiniBank.Domains.Values.Transactions import *
 class Account():
     def __init__(self, acc_id,  owner, balance=0):
     # create an account
         self.acc_id = acc_id
         self.owner = owner
         self.balance = balance
-
-    def get_balance(self):
-        return self.balance
+        self.history = []
 
     def deposit(self, amount):
+        #check if amount is valid
         if(not (amount >= 0)):
-           return False
-        self.balance -= amount
-        return True
+           return None
+
+        #amount is valid, add amount to account balance
+        self.balance += amount
+
+        #Add transaction to account history
+        t = Transaction("deposit", amount)
+        self.history.append(t)
+
+        #Return the transaction
+        return t
 
     def withdraw(self, amount):
+        #check if amount is valid and if there is enough balance
         if(not(amount >= 0) or (not (self.balance >= amount))):
-            return False
-        self.balance += amount
-        return True
+            return None
+
+        #everything is ok, subtract amount from account balance
+        self.balance -= amount
+
+        #Add transaction to account history
+        t = Transaction("withdraw", amount)
+        self.history.append()
+        #Return the transaction
+        return t
