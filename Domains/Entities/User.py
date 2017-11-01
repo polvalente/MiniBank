6ec72151fee2992eec0673a409a2050a1ae4f025
Paulo Valente <polvalente@gmail.com>
2017-11-01 17:34:41 -0200

    Created User class

---
+++ Domains/Entities/User.py
@@ -0,0 +1,13 @@
+from MiniBank.Domains.Entities.Account import *
+
+class User():
+    def __init__(self, uid, name, is_person):
+        self.uid = uid
+        self.name = name
+        self.is_person = is_person
+        self.accounts = []
+
+    def create_account(self, acc_id, balance=0):
+        newAccount = Account(acc_id, self, balance)
+        self.accounts.append(newAccount)
+        return newAccount
